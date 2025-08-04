b141b30dfa191016b493c4feffb00c49b0c74a74
jonghyun.park <jhpark86@gsretail.com>
2025-08-04 14:06:36 +0900

    shell file add

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,8 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ]; then
+	echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ]; then
+	echo "You selected Orange!"
+elif [ $FRUIT == GRAPE ]; the
+	echp "You selected Grape!"
+
